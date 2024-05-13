c484c9c2f1dffbdf646563abe01ec0efca0e6586
Jesse Deda <76166399+jdeda@users.noreply.github.com>
2024-05-13 11:51:38 -0400

    Initial Commit

---
+++ Cookery/CookeryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookeryApp.swift
+//  Cookery
+//
+//  Created by Jesse Deda on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct CookeryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
